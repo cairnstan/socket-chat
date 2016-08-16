68f685ba829c9e2094cd05ec48cdbec5e1e9ce39
cairnstan <cairnsolsen@gmail.com>
2016-08-16 15:25:27 -0500

    Adds emit feature and completes basic chat with sockets.

--- index.js
+++ index.js
@@ -7,13 +7,20 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+// io.on('connection', function(socket){
+//   console.log('a user connected');
+//   socket.on('disconnect', function(){
+//     console.log('user disconnected');
+//   });
+// });
+
 io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+    // console.log('message: ' + msg);
   });
 });
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
-})
+});
